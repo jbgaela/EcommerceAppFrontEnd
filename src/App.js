8a9f5c81579e4ffaa1e4976cd42735f84fe37609
jbgaela <jbgaela@gmail.com>
2021-12-12 03:26:50 +0800

    Revert react router version and added product screen page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -8,18 +8,16 @@ import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} exact />
-            <Route path="/product/:id" element={<ProductScreen />} />
-          </Routes>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </BrowserRouter>
+    </Router>
   )
 }
 
